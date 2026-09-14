fa70ee9073fca34260bd741f21a81eb1a8aee4c0
Юрий Ижбулатов <yv.izbulatov@gmail.com>
2026-09-14 17:01:35 +0300

    Auto 14.09.2026 17:01:35,79

--- js/0_global.js
+++ js/0_global.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     APP_NAME: 'Склад',
     VERSION: '1.0.0',  
-    DEFAULT_USER: 'Иванов',
+    DEFAULT_USER: '',
     SERVER_URL : 'https://script.google.com/macros/s/AKfycbyD8TMsHzm9-RWLCudhdYAeXTI7sVuwfEb2MgCkifwulm8pzIJTQUVRtsxwAi0wajV6tg/exec',
 };
